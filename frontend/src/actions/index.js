3112499511b2423c08450cbc9b0f230ce6820a74
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2017-10-02 17:13:28 +0300

    Setting up actions reducers components

---
+++ frontend/src/actions/index.js
@@ -0,0 +1,13 @@
+//Actions for posts
+export const POST_ADD = 'POST_ADD';
+export const POST_EDIT = 'POST_EDIT';
+export const POST_DELETE = 'POST_DELETE';
+export const POST_LOAD_ALL = 'POST_LOAD_ALL';
+
+//Actions for Comments
+export const COMMENT_ADD = 'COMMENT_ADD';
+export const COMMENT_EDIT = 'COMMENT_EDIT';
+export const COMMENT_DELETE = 'COMMENT_DELETE';
+
+//Actions for category filter
+export const SHOW_ALL = 'SHOW_ALL';
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import readableAppReducer from './reducers';
+import Root from './components/Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  readableAppReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION_ && window.__REDUX_DEVTOOLS_EXTENSION_()
+);
+
+console.log("STORE: ", store.getState());
+
+ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
 registerServiceWorker();
